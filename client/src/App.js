123eb1824293bf5d40561aaadc95f259b8ec9796
narmanguebraun <narmanguebraun1@gmail.com>
2022-05-09 22:57:18 +0200

    add basic form

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className="information">
+        <label>Name:</label>
+        <input type="text" />
+        <label>Age:</label>
+        <input type="number" />
+        <label>Country:</label>
+        <input type="text" />
+        <label>Position:</label>
+        <input type="text" />
+        <label>Wage:</label>
+        <input type="text" />
+        <button>Add employee</button>
+      </div>
     </div>
   );
 }
